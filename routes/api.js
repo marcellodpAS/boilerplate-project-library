f6dc8f4cd86380ce5c63cb935efc687a5e9e176c
marcellodpAS <marcello.depaola@apuliasoft.com>
2024-12-30 08:51:52 +0100

    completed exercise

--- routes/api.js
+++ routes/api.js
@@ -6,42 +6,124 @@
 *       
 */
 
-'use strict';
 
-module.exports = function (app) {
+
+'use strict';
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {type : String, required: true},
+  comments: {type : Array},
+  commentcount:{type : Number,default:0}
+});
+
+const Book = mongoose.model('ItemModel',bookSchema);
+
+module.exports = async function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    .post(async function (req, res){
+      let reqBody = req.body;
+      const { title } = reqBody;
+      try{
+        if ( !title ){
+          return res.send('missing required field title');
+        }
+        const book = new Book({
+        title,
+        comments: [],
+        commentcount: 0
+        });
+        await book.save();
+        return res.json({_id:book._id, title: book.title})
+      } catch(err){
+        console.log(err);
+        return res.status(500).json({error:'Server error'});
+      }
     })
     
-    .post(function (req, res){
+    .get(async function (req, res){
       let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+      try{
+        const items = await Book.find();
+        if (!items) return res.send('no book exists');
+        return res.json(items);
+      }catch(err){
+        return res.status(500).json({error:'Server error'});
+      }
     })
     
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+    .delete(async function(req, res){
+      try{
+        const items = await Book.deleteMany();
+        if(!items) return res.send('no book exists');
+        return res.send('complete delete successful');
+      }catch(err){
+        console.error(err);
+        return res.status(500).json({error:'Server error'});
+      }
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(async function (req, res){
       let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      try{
+        if (!bookid) return res.send('no book exists');
+        
+        const book = await Book.findOne({_id:bookid});
+        if (!book) return res.send('no book exists');
+        if (!book.comments) book.comments = [];
+        return res.json(book);
+      }catch(err){
+        console.log(err);
+        return res.status(500).json({error:'Server error'});
+      }
     })
     
-    .post(function(req, res){
+    .post(async function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
-      //json res format same as .get
+      try{
+        if (!bookid) return res.send('no book exists');
+        
+        if (!comment) return res.send('missing required field comment');
+
+        const book = await Book.findOne({_id : bookid});
+        if(!book) return res.send('no book exists');
+
+        book.comments.push(comment);
+        book.commentcount++;
+        await book.save();
+        return res.send(book);
+      }catch(err){
+        console.error(err);
+        return res.status(500).json({error:'Server error'});
+      }
     })
     
-    .delete(function(req, res){
+    .delete(async function(req, res){
       let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+      try{
+        if(!bookid) return res.send('no book exists');
+        
+
+        const book = await Book.deleteOne({_id:bookid});
+        if(!book.deletedCount) return res.send('no book exists');
+        return res.send('delete successful');
+      }catch(err){
+        console.log(err);
+        return res.status(500).json({error:'Server error'});
+      }
     });
+
+    try{
+      await mongoose.connect(process.env.DB);
+      console.log('connection successful');
+    } catch(err){
+      console.log('connection error')
+    }
   
 };
